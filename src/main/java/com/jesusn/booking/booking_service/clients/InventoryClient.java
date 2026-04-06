2d51a22a06113b6da322db4415f37e9be7e7c44c
jesusnDev <jesusndev@gmail.com>
2026-04-06 10:47:07 -0600

    feat: integrate Swagger/OpenAPI via Springdoc for API documentation

--- src/main/java/com/jesusn/booking/booking_service/clients/InventoryClient.java
+++ src/main/java/com/jesusn/booking/booking_service/clients/InventoryClient.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "inventory-service", url = "http://localhost:8080")
 public interface InventoryClient {
 
-
     @GetMapping("/api/v1/event-seats/{id}")
     EventSeatDTO getEventSeatById(@PathVariable("id") Integer id);
 
